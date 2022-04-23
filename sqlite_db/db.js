be7ecc0834eb93427593d5d47394ea511b41eb54
Smolders Rik,R <riksmol25@gmail.com>
2022-04-23 16:20:16 +0200

    get from api

--- sqlite_db/db.js
+++ sqlite_db/db.js
@@ -2,5 +2,30 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./sqlite_db/prog4eind_sqlitedb.db');
 
 // Get users from shareameal api
+const response = await fetch('https://shareameal-api.herokuapp.com/api/user', {
+    method: 'GET',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+})
+
+var userobj = JSON.parse(response.json());
+for (let index = 0; index < userobj[index] ; index++) {
+    var query = "Insert Into User (ID, Firstname, Lastname, Street, City, isActive, Email, Password, Phonenumber)" +
+    "Values("
+    + userobj[index].id +
+    + userobj[index].firstName +
+    + userobj[index].lastName +
+    + userobj[index].street +
+    + userobj[index].city +
+    + userobj[index].isActive +
+    + userobj[index].emailAdress +
+    + userobj[index].password +
+    + userobj[index].phoneNumber +
+    ")";
+
+    db.run(query)
+}
 
 module.exports = db;
