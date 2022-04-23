36819f398dee5d80fd2ffae422143deb6d3284ca
Smolders Rik,R <riksmol25@gmail.com>
2022-04-23 16:56:58 +0200

    Update db.js

--- sqlite_db/db.js
+++ sqlite_db/db.js
@@ -6,7 +6,7 @@ var db = new sqlite3.Database('./sqlite_db/prog4eind_sqlitedb.db');
 
 // Get users from shareameal api
 const getjson = async url => {
-    const response = await fetch("https://shareameal-api.herokuapp.com/api.user");
+    const response = await fetch("https://shareameal-api.herokuapp.com/api/user");
     if (!response.ok) {
         throw new Error(response.statusText);
     }
