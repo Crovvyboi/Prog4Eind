9860686b51d6aa8d231e7921b1c10cbf60ffdde4
Smolders Rik,R <riksmol25@gmail.com>
2022-07-03 23:38:51 +0200

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 const authController = require('../controllers/auth.controller')
 var router = express.Router();
 
-router.post('/auth/login', authController.validateLogin, authController.login)
+router.post('/login', authController.validateLogin, authController.login)
 
 
 module.exports = router;
